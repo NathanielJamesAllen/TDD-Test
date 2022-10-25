c3bc29e59f6ec084b35d2d168dc1b3d3df24ff8d
punishedchumbucket6000 <nathanieljamesallen2@gmail.com>
2022-10-25 14:42:21 -0400

    Liger

---
+++ animal.py
@@ -0,0 +1,15 @@
+import json
+
+class Animal():
+
+    def __init__(self):
+        self.type = "liger"
+        self.size = "XXL"
+        self.age = 1000000
+        self.name = "Eddie"
+        
+    def speak():
+        pass
+        
+    def describe():
+        pass
