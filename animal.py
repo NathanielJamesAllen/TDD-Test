8b238d4e7f4baac1f79ed78a85c9aa154c921734
punishedchumbucket6000 <nathanieljamesallen2@gmail.com>
2022-11-03 13:34:21 -0400

    update3

--- animal.py
+++ animal.py
@@ -2,14 +2,37 @@ import json
 
 class Animal:
 
-    def __init__(self, type):
-        self.type = type
+    def __init__(self, ctype):
+        self.type = ctype
         self.size = ""
         self.age = 0
         self.name = ""
+
+        if self.type == "cat":
+            self.name = "Seymour"
+        elif self.type == "dog":
+            self.name = "Spot"
+            
+
         
-    def speak():
-        pass
+    def speak(self):
+        if self.type == "cat":
+            if self.size == "small":
+                return "meow"
+            else:
+                return "MEOW!"
+        elif self.type == "dog":
+            if self.size == "small":
+                return "bow wow"
+            elif self.size == "medium":
+                return "Ruff ruff"
+            else:
+                return "arf arf"
+
+
         
-    def describe():
-        pass
+    def describe(self):
+        if self.age < 10:
+            return f"{self.name} is young"
+        elif self.age >= 10:
+            return f"{self.name} is old"
